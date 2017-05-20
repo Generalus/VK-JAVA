0ff34a83a9623105673d3d77f5a23e3a1e71dfad
Denys Bondarenko <pro100denysko@gmail.com>
2017-05-20 19:29:55 +0300

    new

--- javastatistics/src/main/java/com/vksoftware/javastatistics/command/GeneralGroupCommand.java
+++ javastatistics/src/main/java/com/vksoftware/javastatistics/command/GeneralGroupCommand.java
@@ -1,10 +1,10 @@
 package com.vksoftware.javastatistics.command;
 
 
-import com.vksoftware.javastatistics.model.User;
+
 
 public class GeneralGroupCommand implements Command {
-    private User user;
+    //private User user;
 
     public void execute() {
 
--- javastatistics/src/main/java/com/vksoftware/javastatistics/res/Token.java
+++ javastatistics/src/main/java/com/vksoftware/javastatistics/res/Token.java
@@ -5,6 +5,18 @@ public class Token {
     String authorizeUML = "https://oauth.vk.com/authorize?client_id=5490057&display=page&redirect_uri=&scope=friends&response_type=token&v=5.52";
     String log = "https://api.vk.com/method/friends.getOnline?v=5.52&accessToken=";
 
+    public void autorize() {
+        if (accessToken.toString() == null) {
+            try {
+                throw new UnsupportedOperationException("Необходимо указать токен");
+            } catch (UnsupportedOperationException ex) {
+                System.out.println("Необходимо указать токен");
+            }
+        }
+
+
+    }
+
     public String getAccessToken() {
         return accessToken;
     }
