e8a7c243915bb66ef7034c7a7b9771e2adb90b1e
Denys Bondarenko <pro100denysko@gmail.com>
2017-05-22 00:29:05 +0300

    did Singleton for Token

--- javastatistics/src/main/java/com/vksoftware/javastatistics/res/Token.java
+++ javastatistics/src/main/java/com/vksoftware/javastatistics/res/Token.java
@@ -1,14 +1,17 @@
 package com.vksoftware.javastatistics.res;
 
 public class Token {
-    String accessToken="5074ca51d1a7c2527f6190b53dfddd2eade83b8760ac72b9ccb78f5f71d5c313f5c83e4507662acb61d7";
+    private static Token token;
+    private static String accessToken = "";
+
     String authorizeUML = "https://oauth.vk.com/authorize?client_id=5490057&display=page&redirect_uri=&scope=friends&response_type=token&v=5.52";
     String log = "https://api.vk.com/method/friends.getOnline?v=5.52&accessToken=";
 
 
-    public String getAccessToken() {
-        if (accessToken == null) {
-            return accessToken;
+    public static Token getAccessToken() {
+        if (token == null) {
+            token = new Token();
+            return token;
         } else {
             throw new UnsupportedOperationException("Необходимо указать токен");
         }
