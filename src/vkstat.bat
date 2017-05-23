7a69f314b9d10544203d17907be6c1738adf572b
P44elovod <eug.kovtun@gmail.com>
2017-05-24 00:39:47 +0300

    - Коррекция Pom.xml - Приложение .bat файла

---
+++ src/vkstat.bat
@@ -0,0 +1,22 @@
+echo off
+title File Server
+cls
+
+goto start
+
+    write -Dserver_root_folder=... to config root folder
+
+:start
+
+java -jar ^
+ -server ^
+ -Xmx4G ^
+ -Xms1G ^
+ -XX:+AggressiveOpts ^
+ -XX:-UseGCOverheadLimit ^
+ -XX:+DoEscapeAnalysis ^
+ -XX:+HeapDumpOnOutOfMemoryError ^
+ -XX:+UnlockDiagnosticVMOptions ^
+ -XX:+DebugNonSafepoints ^
+  TestServer-1.0-SNAPSHOT-jar-with-dependencies.jar
+pause
